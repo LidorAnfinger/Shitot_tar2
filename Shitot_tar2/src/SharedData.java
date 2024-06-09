71ba5c558a73114fd722b90b4b3caa8414272c32
LidorAnfinger <lidoranf@gmail.com>
2024-06-09 19:33:02 +0300

    JavaDoc HTML created

--- Shitot_tar2/src/SharedData.java
+++ Shitot_tar2/src/SharedData.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 /** SharedData class represent array of integers 
  * @author Lidor Anfinger
- * @version Jun 9, 2024. 
+ * @version June 9, 2024. 
  */
 public class SharedData 
 {
 	/** array of integer, type ArrayList*/
 	private ArrayList<Integer> array;
-	/** array of boolean*/
+	/** array of boolean values*/
 	private boolean [] winArray;
 	/** boolean flag represent found value*/
 	private boolean flag;
@@ -17,49 +17,54 @@ public class SharedData
 	/** Constructor for ShareData class
 	 * @param array				array of integers
 	 * @param b 				represent the number we want to find
-	 * @return					Object of the class
 	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		this.array = array;
 		this.b = b;
 	}
 	
-	/** Getter for the variable winArray*/
+	/** Getter for the variable winArray
+	 * @return 		boolean array
+	 * */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
-	/** Setter for the variable winArray
+	/** Setter for the array winArray
 	 * @param winArray		boolean array of winArray
-	 * @return 				void method
 	 * */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
-	/** Getter for the variable array */
+	/** Getter for the variable array 
+	 * @return 		List that represents the array
+	 * */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
-	/** Getter for the variable b*/
+	/** Getter for the variable b
+	 * @return 		integer
+	 * */
 	public int getB() 
 	{
 		return b;
 	}
 
-	/** Getter for the variable flag*/
+	/** Getter for the variable flag
+	 * @return 			boolean
+	 * */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
 	/** Setter for the variable flag
-	 * @param flag 		boolean value of flag
-	 * @return			void method  
+	 * @param flag 		boolean value of flag 
 	 * */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
